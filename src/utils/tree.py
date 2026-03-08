e2bba2c0a75d7ad0eccdc15e8fc7a1afaf7daff7
Jason <jasonkwok475@gmail.com>
2026-03-08 14:34:50 -0700

    Updated component tree generation to match format

--- src/utils/tree.py
+++ src/utils/tree.py
@@ -1,5 +1,11 @@
-# from google.protobuf import json_format
-# import generated.config.component_pb2 as component
+import os
+import sys
+from pathlib import Path
+from google.protobuf import json_format
+import generated.config.component_pb2 as component
+
+ROOT = os.path.dirname(sys.executable)
+TREE_FILE_NAME = "component_tree.json"
 
 class TreeNode:
   def __init__(self, name, node_id, children=None):
@@ -16,30 +22,37 @@ class TreeUtils:
   def __init__(self):
     pass
 
-  # def generate_component_tree(self) -> tuple[component.ComponentTree, Path]:
-  # # Example of generating a component tree using protobuf
-  # tree_location = Path("./component_tree.json")
+  def generate_component_tree(self, root_node: TreeNode) -> Path:
+    tree_location = Path(ROOT) / TREE_FILE_NAME
 
-  # leaf_component = component.BaseComponent()
-  # leaf_component.name = "RocketDecoder"
-  # leaf = component.Component()
-  # leaf.path = "/components/rocketdecoder"
-  # leaf.tag = "rocketdecoder:v1.0.0"
-  # leaf.id = "RD1"
-  # leaf_component.leaf.CopyFrom(leaf)
+    def build_proto_node(node: TreeNode) -> component.BaseComponent:
+      base = component.BaseComponent()
+      base.name = node.name
 
-  # branch_component = component.ComponentGroup()
-  # branch_component.children.extend([leaf_component])
+      if not node.children: # Leaf
+        leaf = component.Component()
+        leaf.path = node.location or ""
+        leaf.tag = node.hash or "latest"
+        leaf.id = node.id
+        base.leaf.CopyFrom(leaf)
+      else: # Branch
+        branch = component.ComponentGroup()
+        for child in node.children:
+          # Recursively build child BaseComponents
+          proto_child = build_proto_node(child)
+          branch.children.append(proto_child)
+        base.branch.CopyFrom(branch)
+      
+      return base
 
-  # root = component.BaseComponent()
-  # root.name = "FALCON"
-  # root.branch.CopyFrom(branch_component)
+    root_proto_node = build_proto_node(root_node)
 
-  # component_tree = component.ComponentTree()
-  # component_tree.root.CopyFrom(root)
-  # component_tree.version = "1.0.0"
+    component_tree = component.ComponentTree()
+    component_tree.root.CopyFrom(root_proto_node)
+    component_tree.version = "1.0.0"
 
-  # with open(tree_location, "w") as f:
-  #   f.write(json_format.MessageToJson(component_tree))
+    with open(tree_location, "w") as f:
+      json_string = json_format.MessageToJson(component_tree, indent=2)
+      f.write(json_string)
 
-  # return component_tree, tree_location
+    return tree_location
