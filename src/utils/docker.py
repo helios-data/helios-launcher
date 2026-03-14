6543045225cf424c4f2ea685d974621a8e1c3bd0
Jason <jasonkwok475@gmail.com>
2026-03-13 18:14:42 -0700

    Added basic handler functions for docker

--- src/utils/docker.py
+++ src/utils/docker.py
@@ -1,21 +1,64 @@
 import docker
+from .github import GithubUtils
+from .tree import TreeNode
+from config import *
 
-REMOVE_BUILD_INTERMEDIATES = True # Set to False for debugging build issues, True for production use to save space
+# Set to False for debugging build issues, True for production use to save space
+REMOVE_BUILD_INTERMEDIATES = True 
 
 class DockerUtils:
 
   def __init__(self):
     self.client = docker.from_env()
+    self.github_utils = GithubUtils()
 
     if not self.is_docker_running():
       raise RuntimeError("Docker is not running. Please start Docker and try again.")
 
-  def check_image_exists(self) -> None:
-    pass
+  def check_image_exists(self, node: TreeNode) -> bool:
+    list = self.client.images.list(filters={
+      "reference": node.name,
+      #"tag":"Helios"
+      })
+    
+    # Always build the image if the hash = "latest" or anything else
+    expected = {"location": node.location, "type": node.type, "hash": node.hash}
 
-  def build_image(self, repo_url: str) -> None:
-    # Save the repo link, has, and has type into the lables dict
-    self.client.images.build(path=repo_url, tag=repo_url.split('/')[-1].replace('.git', ''), rm=REMOVE_BUILD_INTERMEDIATES)
+    for image in list:
+      match = all(
+        image.labels.get(k) == str(v)
+        for k, v in expected.items()
+      )
+
+      if match:
+        return True
+      
+    return False
+
+  def build_image(self, node: TreeNode) -> None:
+
+    # Clone the repo first if it is of type Github
+    if node.type == Node_Type['GITHUB']:
+      path = ROOT / TEMP_FOLDER / node.name
+      hash = self.github_utils.clone_repo(
+        target_dir=ROOT / TEMP_FOLDER / node.name, 
+        repo_url=node.location, 
+        hash=node.hash
+      )
+    else:
+      path = node.location
+      hash = None
+
+    self.client.images.build(
+      path=path, 
+      tag="helios",
+      labels={
+        "type": node.type,
+        "location": node.location,
+        "hash": hash,
+      },
+      rm=REMOVE_BUILD_INTERMEDIATES
+    )
     pass
 
   def start_container(self) -> None:
